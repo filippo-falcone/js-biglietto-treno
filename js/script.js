6d06cec172f019d2bebae5328d78b0486ae0dc55
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-05 17:25:12 +0100

    feat: add ticket price & final price

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 // Cosa da chiedere al cliente: numero di chilometri da percorrere e età del passeggero
 const userKm = parseInt(prompt('Inserisci il numero di km che vuoi percorrere:'));
-console.log(userKm);
 const userAge = parseInt(prompt("Inserisci l'età del passeggero"));
-console.log(userAge);
 
 // Inserisco i dati dell'utente nell'html
 const userkmPrint = 'Km ' + userKm;
@@ -11,5 +9,17 @@ const userAgePrint = 'età ' + userAge;
 document.getElementById('age').innerHTML = userAgePrint;
 
 // Definire il prezzo del bigletto
+let ticketPrice;
+if (userAge < 18) {
+    ticketPrice = 0.21 * userKm * 20 / 100;
+}
+else if (userAge > 65) {
+    ticketPrice = 0.21 * userKm * 40 / 100;
+}
+else {
+    ticketPrice = 0.21 * userKm;
+}
 
 // Stampare al cliente il prezzo
+const finalPrice = 'Il prezzo finale è: ' + ticketPrice.toFixed(2) + '€';
+document.getElementById('total-price').innerHTML = finalPrice;
